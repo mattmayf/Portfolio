d696ce3e1bb64c58adea2f2dda76c079e80b6f46
mattmayf <mattmayf411@gmail.com>
2019-06-07 11:26:22 -0500

    added unfinished

---
+++ mgmayfield/myprojects/unfinished/New folder/login.php
@@ -0,0 +1,83 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+   
+    <title>Login</title>
+
+    <link rel="canonical" href="https://getbootstrap.com/docs/4.3/examples/checkout/">
+
+    <!-- Bootstrap core CSS -->
+<link href="bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+
+    <style>
+
+      .bd-placeholder-img {
+        font-size: 1.125rem;
+        text-anchor: middle;
+        -webkit-user-select: none;
+        -moz-user-select: none;
+        -ms-user-select: none;
+        user-select: none;
+      }
+
+      @media (min-width: 768px) {
+        .bd-placeholder-img-lg {
+          font-size: 3.5rem;
+        }
+      }
+    </style>
+    <!-- Custom styles for this template -->
+    <link href="form-validation.css" rel="stylesheet">
+  </head>
+  <body>
+  
+   <div class="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-white border-bottom shadow-sm">
+  <h5 class="my-0 mr-md-auto font-weight-normal"><a href="index.html">Unfinished</a></h5>
+  <nav class="my-2 my-md-0 mr-md-3">
+    <a class="p-2 text-dark" href="workoutplan.html">Workout Plan</a>
+    <a class="p-2 text-dark" href="equipment.php">Equipment</a>
+    <a class="p-2 text-dark" href="#">something</a>
+    <a class="p-2 text-dark" href="#">something</a>
+  </nav>
+  <a class="btn btn-outline-primary" href="register.php">Sign up</a>&nbsp;
+  <a class="btn btn-outline-primary" href="login.php">login</a>
+</div>
+
+
+    <div class="container">
+  <div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <h1 class="display-4">Login</h1>
+ </div>
+  
+ 
+ 
+    <div>
+      
+       <form method="post" action="login.php">
+  	    <div class="form-group">
+      <label for="email">User Name or Email:</label>
+      <input type="text" class="form-control" name="username" id="usuername" placeholder="Enter email">
+    </div>
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" name="password" id="password" placeholder="Enter password">
+    </div>
+    <div class="checkbox">
+      <label><input type="checkbox"> Remember me</label>
+    </div>
+    <button type="submit" class="btn btn-default btn-outline-primary">Submit</button>
+  </form>
+
+    </div>
+  </div>
+</div>
+ 
+<br>
+<br><br><br>
+</body>
+</html>
